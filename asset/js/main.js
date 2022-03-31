ff3a6172d68a437994752b9ffe0a9cf6cd81a633
andrea1632 <andrea.bellucci10@libero.it>
2022-03-31 18:03:01 +0200

    fix

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,9 +1,5 @@
 var app = new Vue(
     {
-        created(){
-            startTimer()
-            stopTimer()
-        },
         el: '#root',
         data: {
             immagini: ['https://www.fotografiamoderna.it/wp-content/uploads/2017/12/Fotografie-paesaggi-consigli.jpg', 'https://miro.medium.com/max/1200/1*Gsq1yHsb3cV2JKuj5c_SoQ.jpeg', 'https://www.corsofotografico.com/wp-content/uploads/2020/09/quando-un-teleobiettivo-e%CC%80-meglio-di-un-grandangolo-nella-foto-di-paesaggio-featured.jpg', 'https://www.reterurale.it/flex/images/9/2/3/D.351363a0300d2b7790bd/1.jpg'],
@@ -26,12 +22,12 @@ var app = new Vue(
                     this.index = 0
                 }
             },
-            startTimer(){
-                this.timeout = setInterval(this.nextPhoto, 3000);
-            },
             stopTimer(){
                 clearInterval(this.timeout)
-            }
+            },
+            startTimer(){ this.timeout = setInterval(() => {
+                this.nextPhoto();
+            }, 3000);}
         }
     }
 )
