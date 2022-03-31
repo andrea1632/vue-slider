34bcd954ed6e50a6cf8a6885b01116ba87aa7ae8
andrea1632 <andrea.bellucci10@libero.it>
2022-03-31 16:40:07 +0200

    aggiunto timeout e cambio foto on click

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,9 +1,12 @@
 var app = new Vue(
     {
+        
+
         el: '#root',
         data: {
             immagini: ['https://www.fotografiamoderna.it/wp-content/uploads/2017/12/Fotografie-paesaggi-consigli.jpg', 'https://miro.medium.com/max/1200/1*Gsq1yHsb3cV2JKuj5c_SoQ.jpeg', 'https://www.corsofotografico.com/wp-content/uploads/2020/09/quando-un-teleobiettivo-e%CC%80-meglio-di-un-grandangolo-nella-foto-di-paesaggio-featured.jpg', 'https://www.reterurale.it/flex/images/9/2/3/D.351363a0300d2b7790bd/1.jpg'],
             index: 0,
+            timeout: undefined,
         },
         methods: {
             nextPhoto(){
@@ -20,6 +23,12 @@ var app = new Vue(
                 } else{
                     this.index = 0
                 }
+            },
+            startTimer(){
+                this.timeout = setInterval(this.nextPhoto, 3000);
+            },
+            stopTimer(){
+                clearInterval(this.timeout)
             }
         }
     }
